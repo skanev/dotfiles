8aaceabdaff60830590bb7528d924e141ecda776
Stefan Kanev <stefan.kanev@gmail.com>
2020-09-25 19:41:55 +0300

    tools: hivemind errors in non-opened files
    Visual Studio Code's <F8> is all and great, but it doesn't jump to
    errors in files that have not been opened, because Visual Studio Code
    does not see them.
    
    Not. Any. More.
    
    This script helps make VSCode aware of those errors so the process could
    be sped up quite significantly.
    
    Note that VSCode will eventually support this, but until then - perl to
    the rescue.

--- scripts/track-hivemind.pl
+++ scripts/track-hivemind.pl
@@ -1,5 +1,45 @@
+#!/usr/bin/env perl
 # You can pipe hivemind to this process and it will update the tmux status bar
 # with a status bar fragment that indicates whether it compiles or not.
+#
+# TODO:
+# [ ] Croaks on invalid unicode symbols in while(<>) instead of shlurping them
+#
+# In order to get it working, add the following task in VSCode.
+#
+#     "tasks": {
+#       "version": "2.0.0",
+#       "tasks": [
+#         {
+#           "label": "Track Hivemind",
+#           "type": "shell",
+#           "isBackground": true,
+#           "command": "tail -f -n 0 /tmp/hivemind.log",
+#           "problemMatcher": {
+#             "owner": "typescript",
+#             "fileLocation": [
+#               "relative",
+#               "<PATH-TO-WORKSPACE-(I-M-SURE-IT-CAN-BE-INFERRED-BUT-I-CANT-FIGURE-OUT-HOW)"
+#             ],
+#             "pattern": {
+#               "regexp": "^ERROR in ([^\\s]+) (\\d+):(\\d+)-(?:(\\d+):)?(\\d+) (\\w+):(.*)$",
+#               "file": 1,
+#               "line": 2,
+#               "column": 3,
+#               "endLine": 4,
+#               "endColumn": 5,
+#               "code": 6,
+#               "message": 7
+#             },
+#             "background": {
+#               "activeOnStart": false,
+#               "beginsPattern": "^STARTED$",
+#               "endsPattern": "^(FAILURE|SUCCESS)$"
+#             }
+#           }
+#         }
+#       ]
+#     }
 
 use v5.30;
 use utf8::all;
@@ -11,34 +51,48 @@ use constant SUCCESS => '#[bg=colour236,fg=colour82]';
 use constant WAITING => '#[bg=colour236,fg=colour243]';
 use constant FAILURE => '#[bg=colour124,fg=colour255]';
 
-sub set_status($message) {
-  system "tmux set \@info \"$message\""
+open LOG, '>', '/tmp/hivemind.log';
+
+sub tmux($command) {
+  system "tmux $command";
 }
 
-sub clear_status() {
-  system 'tmux set -u @info';
-}
-
-sub refresh_client() {
-  system "tmux refresh-client -S";
-}
+sub set_status($message) { tmux "set \@info \"$message\""; }
+sub clear_status()       { tmux 'set -u @info'; }
+sub refresh_client()     { tmux 'refresh-client -S'; }
 
 set_status '⏳';
 
-$SIG{INT} = sub { system 'tmux set -u @info'; };
+$SIG{INT} = sub { clear_status };
 
 my $errors = 0;
 my $last_failed = 0;
+my $log_next_line = 1;
 
 while (<>) {
   print;
+
   $_ = colorstrip $_;
 
   next unless /^webpack +\| (.*)$/;
 
   $_ = $1;
 
-  $errors++ if /^ERROR in/;
+  if (/^ERROR in/) {
+    $errors++;
+    print LOG "$_ ";
+    $log_next_line = 1;
+  } elsif ($log_next_line) {
+    $log_next_line = 0;
+
+    if (/^SyntaxError: [^:]+: (.*) \((\d+):(\d+)\)/) {
+      say LOG "$2:$3-$3 SyntaxError: $1";
+    } elsif (/^Module build failed/) {
+      say LOG "1:1-2 $_";
+    } else {
+      say LOG $_;
+    }
+  }
 
   next unless /｢wdm｣: (.*)/;
 
@@ -46,16 +100,19 @@ while (<>) {
     when('Failed to compile.') {
       $last_failed = 1;
       set_status "${\FAILURE} 💥 $errors";
+      say LOG 'FAILURE';
     }
     when('Compiling...') {
       $errors = 0;
       my $prefix = $last_failed ? FAILURE : WAITING;
       set_status "$prefix …";
+      say LOG 'STARTED';
     }
     when('Compiled successfully.') {
       $errors = 0;
       $last_failed = 0;
       set_status "${\SUCCESS} ✔︎";
+      say LOG 'SUCCESS';
     }
     default { next }
   }
@@ -63,4 +120,3 @@ while (<>) {
 }
 
 clear_status;
-
