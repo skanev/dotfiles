558e7fb27cac8ac008470d58e51e10b3aa70c0bd
Stefan Kanev <stefan.kanev@gmail.com>
2020-09-25 20:59:37 +0300

    tools: add a scratchpad directory

---
+++ scratchpad/README.markdown
@@ -0,0 +1,4 @@
+# Sratchpad
+
+A junk drawer of sorts, where I can keep various small scripts that I would usually discard, but I
+might find useful eventually.
---
+++ scratchpad/sonokai-colors.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+
+# Generate a dim versions of the sonokai colors I can use for perlPOD to distinguish from actual perl code.
+
+use v5.30;
+use utf8::all;
+use strict;
+
+use List::Util qw(min max);
+
+use Graphics::Color;
+use Graphics::Color::RGB;
+use Graphics::Color::HSV;
+
+my %codes = (
+  Red => 'f85e84',
+  Orange => 'ef9062',
+  Purple => 'ab9df2',
+  Yellow => 'e5c463',
+  White => 'ffffff',
+  Green => '9ecd6f',
+);
+
+while (my ($name, $code) = each %codes) {
+  my $hsv = Graphics::Color::RGB->from_hex_string($code)->to_hsv;
+  $hsv->value( max(0.0, $hsv->v - 0.32) );
+  $hsv->saturation( max(0.0, $hsv->s - 0.33) );
+  my $result = $hsv->to_rgb->as_hex_string;
+  say "  hi Dim$name guifg=#$result";
+}
