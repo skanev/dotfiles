eb5e41e16ca1d6cd78abdd00a08f844bd80c0133
Stefan Kanev <stefan.kanev@gmail.com>
2021-07-24 14:54:09 +0300

    nvim: fix toggling signature help

--- vim/lua/mine/init.lua
+++ vim/lua/mine/init.lua
@@ -1,17 +1,38 @@
 local M = {}
 
-function M.toggle_signature_help()
+local function signature_help_window()
   local success, result = pcall(vim.api.nvim_buf_get_var, 0, 'saga_signature_help_win')
 
   if not success then return end
 
   local winid = result[1]
 
-  if not success or not winid then return end
+  if not winid then return end
 
   if vim.api.nvim_win_is_valid(winid) then
+    return winid
+  else
+    return
+  end
+end
+
+function M.hide_signature_help()
+  local winid = signature_help_window()
+
+  if winid then
+    vim.api.nvim_win_close(winid, true)
+  end
+end
+
+function M.toggle_signature_help()
+  local winid = signature_help_window()
+
+  if winid then
     vim.api.nvim_win_close(winid, true)
   else
+    if vim.fn.pumvisible() == 1 then
+      vim.fn.complete(vim.fn.col('.'), {})
+    end
     require('lspsaga.signaturehelp').signature_help()
   end
 end
