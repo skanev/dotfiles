4dca6438564341964bbf3d43b324358690ca2daf
Stefan Kanev <stefan.kanev@gmail.com>
2022-05-06 10:03:28 +0300

    mire: add super_diff
    It's useful when looking at errors when comparing complicated
    structures. For the time being I can just comment/uncomment it, because
    it's not a default I like, but I'll need to figure out something smarter
    eventually.

--- support/ruby/mire/spec/spec_helper.rb
+++ support/ruby/mire/spec/spec_helper.rb
@@ -2,6 +2,7 @@ $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 
 require 'rspec'
 require 'mire'
+#require 'super_diff/rspec'
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
