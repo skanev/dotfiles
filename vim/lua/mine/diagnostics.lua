f7cd8bfa2e4eac8e53bc2c8806752fc8b7ae0f37
Stefan Kanev <stefan.kanev@gmail.com>
2022-06-02 00:21:02 +0300

    nvim: improve cycle_diagnostics
    <D-m> can switch between different diagnostic levels from the LSP. I had
    a hacky way to switch prior to the nice stuff in nvim 0.5.0+, but now I
    can do it sufficiently less hacky and sufficiently more reliably.
    
    Also, my lua knowledge has improved a bit, so fix the global variable
    shenanigans.

--- vim/lua/mine/diagnostics.lua
+++ vim/lua/mine/diagnostics.lua
@@ -1,58 +1,32 @@
 local util = require('mine.util')
-local log = require('mine.log')
 
-_I = _I or {}
-_I.last_diagnostics = _I.last_diagnostics or {}
-_I.diagnostic_level = _I.diagnostic_level or 'full'
+_G._Mine = _G._Mine or {}
+_Mine.diagnostic_level = _Mine.diagnostic_level or 'full'
 
 local levels = { 'full', 'light', 'none' }
 
 local function set_diagnostic_level(level)
-  local original_handler = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics, {
-      virtual_text = true,
-      signs = true,
-      underline = true,
-      update_in_insert = false,
-    }
-  )
+  local severity
 
-  local handler = function(err, result, context, config)
-    -- TODO worth filtering for current buffer only
-    _I.last_diagnostics[vim.api.nvim_get_current_buf()] = { err, result, context, config }
-    --log.info(vim.inspect({ err, result, context, config }))
-
-    result = util.shallow_copy(result)
-
-    result.diagnostics = result.diagnostics and util.filter(result.diagnostics, function(item)
-      if level == 'none' then
-        return false
-      elseif level == 'light' then
-        return item.severity == 1
-      elseif level == 'full' then
-        return true
-      else
-        log.warn("Unknown severity level " .. tostring(level))
-        return true
-      end
-    end)
-
-    original_handler(err, result, context, config)
+  if level == 'full' then
+    severity = { min = vim.diagnostic.severity.HINT }
+  elseif level == 'light' then
+    severity = { min = vim.diagnostic.severity.ERROR }
+  elseif level == 'none' then
+    severity = { min = vim.diagnostic.severity.ERROR, max = vim.diagnostic.severity.HINT }
   end
 
-  vim.lsp.handlers["textDocument/publishDiagnostics"] = handler
+  vim.diagnostic.config {
+    underline = { severity = severity },
+    virtual_text = { severity = severity },
+    signs = { severity = severity },
+  }
 
-  local last_diagnostics = _I.last_diagnostics[vim.api.nvim_get_current_buf()]
-
-  if last_diagnostics then
-    handler(unpack(last_diagnostics))
-  end
-
-  _I.diagnostic_level = level
+  _Mine.diagnostic_level = level
 end
 
 local function cycle_diagnostics()
-  local index = util.index_of(levels, _I.diagnostic_level or 'full')
+  local index = util.index_of(levels, _Mine.diagnostic_level or 'full')
   local next_index
 
   if index == #levels then
@@ -62,12 +36,12 @@ local function cycle_diagnostics()
   end
 
   local next_level = levels[next_index]
-  vim.cmd(string.format("echo 'diagnostic level: %s'", next_level))
+  print(string.format("diagnostic level: %s", next_level))
   set_diagnostic_level(next_level)
 end
 
 return {
   set_diagnostic_level = set_diagnostic_level,
   cycle_diagnostics = cycle_diagnostics,
-  diagnostic_level = function() return _I.diagnostic_level end
+  diagnostic_level = function() return _Mine.diagnostic_level end
 }
