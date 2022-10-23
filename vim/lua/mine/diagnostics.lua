5ad0c34d68d2ffc62871ec00f1545987f677ae79
Stefan Kanev <stefan.kanev@gmail.com>
2022-10-23 15:11:39 +0300

    nvim: fix diagnostics
    goto_prev and goto_next have stopped working. Had to make them work by
    going through vim.diagnostics.goto_next(), but that now has a separate
    issue of not respecting my diagnostic level, which I have also fixed.
    
    Also, updated a few other things I should have updated.

--- vim/lua/mine/diagnostics.lua
+++ vim/lua/mine/diagnostics.lua
@@ -5,16 +5,18 @@ _Mine.diagnostic_level = _Mine.diagnostic_level or 'full'
 
 local levels = { 'full', 'light', 'none' }
 
-local function set_diagnostic_level(level)
-  local severity
-
+local function severity_for_level(level)
   if level == 'full' then
-    severity = { min = vim.diagnostic.severity.HINT }
+    return { min = vim.diagnostic.severity.HINT }
   elseif level == 'light' then
-    severity = { min = vim.diagnostic.severity.ERROR }
+    return { min = vim.diagnostic.severity.ERROR }
   elseif level == 'none' then
-    severity = { min = vim.diagnostic.severity.ERROR, max = vim.diagnostic.severity.HINT }
+    return { min = vim.diagnostic.severity.ERROR, max = vim.diagnostic.severity.HINT }
   end
+end
+
+local function set_diagnostic_level(level)
+  local severity = severity_for_level(level)
 
   vim.diagnostic.config {
     underline = { severity = severity },
@@ -40,8 +42,16 @@ local function cycle_diagnostics()
   set_diagnostic_level(next_level)
 end
 
+local function goto_diagnostic(direction)
+  local severity = severity_for_level(_Mine.diagnostic_level)
+
+  vim.diagnostic[direction]({ severity = severity })
+end
+
 return {
   set_diagnostic_level = set_diagnostic_level,
   cycle_diagnostics = cycle_diagnostics,
-  diagnostic_level = function() return _Mine.diagnostic_level end
+  diagnostic_level = function() return _Mine.diagnostic_level end,
+  goto_prev = function() goto_diagnostic('goto_prev') end,
+  goto_next = function() goto_diagnostic('goto_next') end,
 }
