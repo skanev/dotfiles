5fc938d91bf2cc1a39e17167607fa77fc76c27dd
Stefan Kanev <stefan.kanev@gmail.com>
2022-11-26 12:25:10 +0200

    nvim: lua snippets
    * A nicer lreq
    * Usually I don't care about the index when going over with fori

--- vim/snippets/luasnippets/lua.lua
+++ vim/snippets/luasnippets/lua.lua
@@ -12,6 +12,17 @@ local fmt = require("luasnip.extras.fmt").fmt
 --local rep = require("luasnip.extras").rep
 
 return {
+  s('lreq', fmt([[local {} = require('{}')]], {
+    d(2, function(args)
+      local parts = vim.split(args[1][1], '.', { plain = true })
+      local name = parts[#parts]
+      if name == '' then
+        name = 'name'
+      end
+      return sn(nil, i(1, name))
+    end, { 1 }),
+    i(1, 'package')
+  })),
   s('unload', fmt([[package.loaded['{}'] = nil]], {
     d(1, function()
       local name = vim.fn.matchstr(vim.fn.expand('%'), [[\<lua/\zs.*\ze\.lua]])
