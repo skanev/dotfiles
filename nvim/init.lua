e8f857d65b800ec98b485112f037587286c440ed
Stefan Kanev <stefan.kanev@gmail.com>
2023-05-21 17:28:38 +0300

    nvim: init.vim -> init.lua
    Not really necessary, but I've been using neovim for years now, and it's
    about time I get deeper into configuration via lua. Plus, I needed this
    when I started to fiddle with firenvim.

---
+++ nvim/init.lua
@@ -0,0 +1,17 @@
+--TODO Is this necessary or can it be removed?
+vim.g.dotfiles_dir = vim.fn.expand('~/code/personal/dotfiles')
+
+vim.opt.runtimepath:prepend('~/.vim/after')
+vim.opt.runtimepath:prepend('~/.vim')
+
+vim.o.packpath = vim.o.runtimepath
+
+local function best_match(pattern)
+  return vim.fn.reverse(vim.fn.sort(vim.fn.glob(pattern, true, true)))[1]
+end
+
+vim.g.python_host_prog = best_match('~/.pyenv/versions/*/envs/neovim-python2/bin/python')
+vim.g.python3_host_prog = best_match('~/.pyenv/versions/*/envs/neovim-python3/bin/python')
+vim.g.ruby_host_prog = best_match('~/.rbenv/versions/*/bin/neovim-ruby-host')
+
+vim.cmd [[source ~/.vimrc]]
